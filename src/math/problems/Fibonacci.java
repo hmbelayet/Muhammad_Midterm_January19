6eb1c80c98db2c03f77cf45c52f6480453b608a2
People Tech <peoplet91@gmail.com>
2019-03-02 18:57:03 -0500

    Fibinacci

--- src/math/problems/Fibonacci.java
+++ src/math/problems/Fibonacci.java
@@ -6,7 +6,14 @@ public class Fibonacci {
           Write 40 Fibonacci numbers with java.
           0,1,1,2,3,5,8,13
          */
-
-
+        int n, a = 0, b = 0, c = 1;
+        System.out.print("40 Fibonacci numbers: ");
+        for(int i = 1; i <= 40; i++)
+        {
+            a = b;
+            b = c;
+            c = a + b;
+            System.out.print(a+", ");
+        }
     }
 }
